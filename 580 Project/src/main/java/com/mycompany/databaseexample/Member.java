e839f32547d04afae738c1f194ea6c7b4bd2ba67
Jessie G <jessiel.gullo@gmail.com>
2023-10-05 11:26:18 -0400

    changed to code with menu bar

---
+++ 580 Project/src/main/java/com/mycompany/databaseexample/Member.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.databaseexample;
+
+/**
+ *
+ * @author jessi
+ */
+public class Member {
+    
+    private int memNum;
+    private String name;
+
+    /**
+     * @return the memNum
+     */
+    public int getMemNum() {
+        return memNum;
+    }
+
+    /**
+     * @param memNum the memNum to set
+     */
+    public void setMemNum(int memNum) {
+        this.memNum = memNum;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
